f0011b6e6e3a116f08adf77e2603ce555435fe3c
Francesco De Magistris <francescomaria.demagistris@eng.it>
2019-12-05 11:13:05 +0100

    Commento

--- src/MainPersona.java
+++ src/MainPersona.java
@@ -46,7 +46,7 @@ public class MainPersona {
                         }
                     }
                     break;
-
+                    //qwertyuiopasdfghjklzxcvbnm
             }
             }
             while (scelta != 4) ;
